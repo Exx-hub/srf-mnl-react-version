e454b13e44a8b26a01566a27836e45f0da6480e4
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-07 15:10:43 +0800

    fix div getting smaller when rescaling down, no elements should be protruding

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 	return (
 		<div>
 			<NavBar />
-			<Container>
+			<Container fluid>
 				<Row>
 					<Col>
 						<Home />
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,16 +4,19 @@ import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
 	return (
-		<Navbar bg="dark" variant="dark">
+		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Navbar.Brand href="#home">SRF MNL</Navbar.Brand>
-			<Nav className="ml-auto">
-				<Nav.Link href="#home">HOME</Nav.Link>
-				<Nav.Link href="#courses">COURSES</Nav.Link>
-				<Nav.Link href="#profile">PROFILE</Nav.Link>
-				<Nav.Link href="#register">REGISTER</Nav.Link>
-				<Nav.Link href="#login">LOG IN</Nav.Link>
-				<Nav.Link href="#logout">LOG OUT</Nav.Link>
-			</Nav>
+			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+			<Navbar.Collapse id="responsive-navbar-nav">
+				<Nav className="ml-auto">
+					<Nav.Link href="#home">HOME</Nav.Link>
+					<Nav.Link href="#courses">COURSES</Nav.Link>
+					<Nav.Link href="#profile">PROFILE</Nav.Link>
+					<Nav.Link href="#register">REGISTER</Nav.Link>
+					<Nav.Link href="#login">LOG IN</Nav.Link>
+					<Nav.Link href="#logout">LOG OUT</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
 		</Navbar>
 	);
 }
