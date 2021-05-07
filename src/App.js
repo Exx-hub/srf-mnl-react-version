2cbd8b1ccd57802311b3621743e23b08df8d8caf
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-07 11:36:54 +0800

    Create navbar component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
+import Container from "react-bootstrap/Container";
+
 function App() {
 	return (
 		<div>
 			<NavBar />
+			<Container />
 		</div>
 	);
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 function NavBar() {
-	return <div>NAVBAR</div>;
+	return (
+		<Navbar bg="dark" variant="dark">
+			<Navbar.Brand href="#home">SRF MNL</Navbar.Brand>
+			<Nav className="ml-auto">
+				<Nav.Link href="#home">HOME</Nav.Link>
+				<Nav.Link href="#courses">COURSES</Nav.Link>
+				<Nav.Link href="#profile">PROFILE</Nav.Link>
+				<Nav.Link href="#register">REGISTER</Nav.Link>
+				<Nav.Link href="#login">LOG IN</Nav.Link>
+				<Nav.Link href="#logout">LOG OUT</Nav.Link>
+			</Nav>
+		</Navbar>
+	);
 }
 
 export default NavBar;
