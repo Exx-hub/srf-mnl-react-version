43a97281769328aa288ae1e93ff7d4e2c860d7f0
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-10 12:27:44 +0800

    Install react router,create routes for home and courses

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./App.css";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -11,18 +12,22 @@ import Footer from "./components/Footer";
 
 function App() {
 	return (
-		<div>
-			<NavBar />
-			<Container fluid>
-				<Row>
-					<Col>
-						<Home />
-						{/* <Courses /> */}
-					</Col>
-				</Row>
-			</Container>
-			<Footer />
-		</div>
+		<Router>
+			<div>
+				<NavBar />
+				<Switch>
+					<Container fluid>
+						<Row>
+							<Col>
+								<Route exact path="/" component={Home} />
+								<Route path="/courses" component={Courses} />
+							</Col>
+						</Row>
+					</Container>
+				</Switch>
+				<Footer />
+			</div>
+		</Router>
 	);
 }
 
