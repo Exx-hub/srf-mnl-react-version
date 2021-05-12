0da893f73ba6ee8ab2ce6826fdf024a4fc3100a3
Exx-hub <alvinfloresacosta@gmail.com>
2021-05-12 12:11:29 +0800

    update: Style register and login forms

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,23 @@ import React from "react";
 
 function Login() {
 	return (
-		<div className="login">
-			<h2>LOGIN</h2>
-			<form className="loginForm">
-				<input type="email" placeholder="Email Address" />
-				<input type="password" placeholder="Password" />
-				<Button>Sign In</Button>
-				<a href="/register">Not yet registered? Sign up now!</a>
+		<div className="login col-md-5 mr-auto ml-auto">
+			<h2 className="text-center mt-3">LOGIN</h2>
+			<form className="loginForm mt-3">
+				<input
+					className="my-2 p-2 siteInput"
+					type="email"
+					placeholder="Email Address"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="password"
+					placeholder="Password"
+				/>
+				<Button className="my-3 siteButton">Sign In</Button>
+				<a href="/register" className="text-center">
+					Not yet registered? Sign up now!
+				</a>
 			</form>
 		</div>
 	);
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,16 +3,40 @@ import Button from "react-bootstrap/Button";
 
 function Register() {
 	return (
-		<div className="login">
-			<h2>SIGN UP</h2>
-			<form className="loginForm">
-				<input type="text" placeholder="Firstname" />
-				<input type="text" placeholder="Lastname" />
-				<input type="number" placeholder="09XX-XXX-XXXX" />
-				<input type="email" placeholder="Email Address" />
-				<input type="password" placeholder="New Password" />
-				<input type="password" placeholder="Confirm Password" />
-				<Button>Sign Me Up</Button>
+		<div className="register login col-md-5 mr-auto ml-auto">
+			<h2 className="text-center mt-3">SIGN UP</h2>
+			<form className="registerForm">
+				<input
+					className="my-2 p-2 siteInput"
+					type="text"
+					placeholder="Firstname"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="text"
+					placeholder="Lastname"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="number"
+					placeholder="09XX-XXX-XXXX"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="email"
+					placeholder="Email Address"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="password"
+					placeholder="New Password"
+				/>
+				<input
+					className="my-2 p-2 siteInput"
+					type="password"
+					placeholder="Confirm Password"
+				/>
+				<Button className="my-3 siteButton">Sign Me Up</Button>
 			</form>
 		</div>
 	);
